4fbf52367dbb8cd180d98d34264430bf704b4611
Mane <mane.ghudratyan@gmail.com>
2024-02-02 19:51:34 +0400

    Create index.py in it write python code, write commands in the Dockerfile which is help to run it

---
+++ Docker/python-application/index.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+def main():
+    arr = [6,10, 12, 4, 55, 132, 222, 111, 970]
+    print("Original array:", arr)
+    bubble_sort(arr)
+    print("Sorted array:", arr)
+
+if __name__ == "__main__":
+    main()
+
